e3b5912692858404d1dacb3fa388d29a53b37bcb
or <orzeharia85@gmail.com>
2023-03-11 15:39:58 +0200

    dbb

--- db/db.js
+++ db/db.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2");
-const dbConfig = require("./db.config.js");
+//const mysql = require("mysql2");
+var connection =mysql.createConnection("mysql://b8d5b31af359da:d0dbfb07@us-cdbr-east-06.cleardb.net/heroku_33ce5531588afeb?reconnect=true");
+//const dbConfig = require("./db.config.js");
 // Create a connection to the database
-mysql://b8d5b31af359da:d0dbfb07@us-cdbr-east-06.cleardb.net/heroku_33ce5531588afeb?reconnect=true
 
 const connection = mysql.createConnection({
     host: dbConfig.HOST,

7fce1c68ca4d4cd73a8d3d267d3ad535f3c91b7a
or <orzeharia85@gmail.com>
2023-03-11 15:34:05 +0200

    db

--- db/db.config.js
+++ db/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "mysql"
+    HOST: "us-cdbr-east-06.cleardb.net",
+    USER: "b8d5b31af359da",
+    PASSWORD: "d0dbfb07",
+    DB: "heroku_33ce5531588afeb"
 };

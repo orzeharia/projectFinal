233fe137aa4ab20d3d3052442241e238ee0b670b
anatez <anatezra304@gmail.com>
2023-04-01 15:51:41 +0300

    FIX SQL

--- db/db.js
+++ db/db.js
@@ -10,8 +10,8 @@ const connection = mysql.createPool({
     database: dbConfig.DB
 });
 // open the MySQL connection
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
-});
+// connection.connect(error => {
+//     if (error) throw error;
+//     console.log("Successfully connected to the database.");
+// });
 module.exports = connection;

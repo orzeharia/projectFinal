3cab2a5c7cebfcbae54442d9154fe84e6ce90474
anatez <anatezra304@gmail.com>
2023-04-01 15:56:42 +0300

    FIX SQL

--- db/db.js
+++ db/db.js
@@ -8,7 +8,7 @@ const connection = mysql.createPool({
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB
-});
+}); 
 // open the MySQL connection
 // connection.connect(error => {
 //     if (error) throw error;
